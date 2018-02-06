201be6aa5eb6d270c7c7e4e15abbf6daa673ba67
Tara <tara@um0.dk>
2018-02-06 12:07:31 +0100

    all but last assignment

--- Solar system/Assets/scripts/MeteorBehavior.cs
+++ Solar system/Assets/scripts/MeteorBehavior.cs
@@ -15,14 +15,24 @@ public class MeteorBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float distance = Vector3.Distance(Earth.transform.position, Meteor.transform.position);
-        float RadiusEarth = Earth.transform.localScale.x / 2;
-        float RadiusMeteor = Meteor.transform.localScale.x / 2;
         
-        Debug.Log(distance + " : " + Earth.transform.position.y + " : " + Meteor.transform.position.y);
+        float distance = Vector3.Distance(Earth.transform.position, Meteor.transform.position);
+        //        float RadiusEarth = Earth.transform.localScale.x / 2;
+        float RadiusEarth = 0.03f;
+        //        float RadiusMeteor = Meteor.transform.localScale.x / 2;
+        float RadiusMeteor = 0.0375f;
+
+        //Debug.Log(distance - (RadiusEarth + RadiusMeteor));
+        Debug.Log(GameObject.FindGameObjectWithTag("Sun").transform.localScale.x);
 
         if (distance < RadiusEarth + RadiusMeteor) {
             Debug.Log("COLLISION!!!");
+            Explode();
         }
 	}
+
+    void Explode() {
+        ParticleSystem explosion = GetComponent<ParticleSystem>();
+        explosion.Play();
+    }
 }
