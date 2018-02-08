262af5966f12251dc163fd8ee10e1b1b02f8d081
Shuxiao <huangshuxiao.gl@gmail.com>
2018-02-08 17:03:59 +0100

    half way

---
+++ Handin 1/Star Wars/Assets/Scripts/BaseChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseChanger : MonoBehaviour {
+
+	GameObject spaceShuttle;
+	GameObject earth;
+
+	// Use this for initialization
+	void Start () {
+		spaceShuttle = GameObject.Find("IT_Space_Shuttle");
+		earth = GameObject.Find("IT_Earth");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnGUI()
+	{
+		GUI.color = Color.red;
+		GUI.Label (new Rect (10, 10, 500, 100), "LocalPosition space shuttle: " + spaceShuttle.transform.localPosition);
+		GUI.Label (new Rect (10, 30, 500, 100), "LocalPosition earth: " + earth.transform.localPosition);
+
+		// forward dotted with right = 0 if two forward vectors are aligned
+//		GUI.Label (new Rect (10, 50, 500, 100), "DotProduct forward: " + Vector3.Dot(path.transform.forward, shuttle.transform.right));
+
+	}
+}
